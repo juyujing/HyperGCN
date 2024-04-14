675e03eea5edcd3f590d1bf5ec2f02d3eefbe757
juyujing <162807321+juyujing@users.noreply.github.com>
2024-04-15 04:11:52 +0900

    Delete hyper_motif directory

--- hyper_motif/cuda_memory_release.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cuda_runtime_api.h>
-
-void release_cuda_memory(void* device_ptr) {
-    cudaError_t status = cudaFree(device_ptr);
-    if (status != cudaSuccess) {
-        fprintf(stderr, "Failed to release CUDA memory: %s\n", cudaGetErrorString(status));
-    }
-}
